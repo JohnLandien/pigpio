6c7e7f18741c30df08e684688679c6a0c99a0437
John Langhout <JohnLandien@users.noreply.github.com>
2023-08-03 23:41:58 +0200

    Update alert.js

--- example/alert.js
+++ example/alert.js
@@ -37,7 +37,7 @@ const watchPWM = ( pwmSource) => {
     }
     if (diff1 >> 0 && diff2 >> 0) {
       pwmSignal = Math.floor(100*diff1/(diff1 + diff2));
-      if ( pwmSignal <= 10% ) { console.log("pump is running on max speed")}
+      if ( pwmSignal <= 10 ) { console.log("pump is running on max speed")}
       elseif ( pwmSignal > 10 && pwmSignal <= 84 ) { console.log("pump is running on " ,  ((pwmSignal-10)*1/0,74) + " % of max speed)}
       elseif ( pwmSignal > 84 && pwmSignal <= 91 ) { console.log("pump is running on lowest speed")}
       elseif ( pwmSignal > 91 && pwmSignal <= 95 ) { console.log("pump is switchting on / off")}
