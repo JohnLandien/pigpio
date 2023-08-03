b5723e70f2d09a40eb7dd55a2af54c7b00a5ab33
John Langhout <JohnLandien@users.noreply.github.com>
2023-08-03 21:27:58 +0200

    Update alert.js

--- example/alert.js
+++ example/alert.js
@@ -1,6 +1,17 @@
 'use strict';
 
-// Assumption: the LED is off when the program is started
+/*Read Incoming PWM signal from Grundfoss UPM 3 pump 
+  Signal is from an optocoupler limitted to 36V with a zenerdiode and a 470 Ohm resistor is between collector and output signal.
+
+  PWM 
+         =< 10% pump is running om maximum speed
+  > 10 / =< 84 % Variable speed from maximum speed to minimum speed
+  > 84 / =< 91 % Minimum speed
+  > 91 / =< 95 % Hysteresis area on/off
+  > 95 / =< 100 % Standby mode => off
+  
+
+*/
 
 const Gpio = require('../').Gpio;
 
@@ -10,7 +21,7 @@ const pwminput = new Gpio(14, {
 });
 
 const watchPWM = () => {
-  let startTick1, startTick2,diff1,diff2;
+  let startTick1, startTick2,diff1,diff2,pwmSignal;
 
   // Use alerts to determine how long the LED was turned on
   pwminput.on('alert', (level, tick) => {
@@ -23,19 +34,19 @@ const watchPWM = () => {
       diff1 = (tick >> 0) - (startTick1 >> 0); // Unsigned 32 bit arithmetic
       //console.log(diff1);
     }
-    if (diff1 >> 0 && diff2 >> 0) { 
-      console.log( "dutycycle = ", Math.floor(100*diff1/(diff1 + diff2)) + "%")
+    if (diff1 >> 0 && diff2 >> 0) {
+      pwmSignal = Math.floor(100*diff1/(diff1 + diff2));
+      if ( pwmSignal =< 10% ) { console.log("pump is running on max speed")}
+      elseif ( pwmSignal > 10 && pwmSignal =< 84 ) { console.log("pump is running on " ,  ((pwmSignal-10)*1/0,74) + " % of max speed)}
+      elseif ( pwmSignal > 84 && pwmSignal =< 91 ) { console.log("pump is running on lowest speed")}
+      elseif ( pwmSignal > 91 && pwmSignal =< 95 ) { console.log("pump is switchting on / off")}
+      elseif ( pwmSignal > 95 && pwmSignal =< 100 ) { console.log("pump is standby and not working")}
+      else { console.log("pump has no pwm signal")}
+      
     }
   });
     
 };
 
-//setInterval(() => {
-  watchPWM();
-//}, 5000);
-
-
-
-// Turn the LED on for 15 microseconds once per second
 
 
