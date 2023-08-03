d052cbe6371d85c395b91f564a3b45322fab5321
John Langhout <JohnLandien@users.noreply.github.com>
2023-08-03 20:58:20 +0200

    Update alert.js

--- example/alert.js
+++ example/alert.js
@@ -23,7 +23,8 @@ const watchPWM = () => {
       diff1 = (tick >> 0) - (startTick1 >> 0); // Unsigned 32 bit arithmetic
       //console.log(diff1);
     }
-    
+    if (diff1 >> 0 && diff2 >> 0) { 
+    console.log( "dutycycle = ", diff1/(diff1 + diff2))
     
   });
   if (diff1 >> 0 && diff2 >> 0) { 
